aeb45128277ef2f9f721eafde5cec9587a0c9c57
Bilal Bai <yue.bai@avenir.hk>
2024-08-05 18:37:31 -0400

    stress test a sample crypto portfolio

---
+++ examples/crypto/deribit.py
@@ -0,0 +1,49 @@
+import requests
+import pandas as pd
+
+# Get list of instrument from deribit
+
+url = 'https://deribit.com/api/v2/public/get_instruments'
+coins = ['BTC', 'ETH']
+expiry_dates = ['4JUL24', '26JUL24', '29JUL24', '30AUG24', '27SEP24','30DEC24']
+
+def get_instruments(url = url, coins = coins, expiry_dates = expiry_dates):
+  instruments = []
+  for coin in coins:
+    r = requests.get(url, params = {'currency': coin,
+                                    'expired': 'false',
+                                    'kind': 'option'})
+    for result in r.json()['result']:
+      if any(x in result['instrument_name'] for x in expiry_dates):
+        instruments.append(result['instrument_name'])
+  print(f'{len(instruments)} instruments have been crawled.')
+  return instruments
+
+
+
+# Get the order book from deribit
+url = 'https://deribit.com/api/v2/public/get_order_book'
+depth = 10
+attributes = ['underlying_price', 'timestamp', 'mark_iv', 'instrument_name',
+              'index_price', 'greeks', 'bid_iv', 'ask_iv', 'best_bid_price', 'best_ask_price']
+
+def get_order_book(instruments,url = url,depth = depth,attributes = attributes):
+  data = {}
+  for instrument in instruments:
+    r = requests.get(url, params = {'depth': depth,
+                                    'instrument_name': instrument})
+    c = r.json()['result']
+    for attribute in attributes:
+      if attribute == 'greeks':
+        for greek in c[attribute].keys():
+          if greek not in data.keys():
+            data[greek]= [c[attribute][greek]]
+          else:
+            data[greek].append(c[attribute][greek])
+      else:
+        if attribute not in data.keys():
+          data[attribute]= [c[attribute]]
+        else:
+          data[attribute].append(c[attribute])
+  data = pd.DataFrame(data)
+  return data
