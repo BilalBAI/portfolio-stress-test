c8e91af377d3d8338afc655f0007cfdf8a508a7c
Bilal <bilal.yue.bai@gmail.com>
2025-05-15 11:11:17 +0800

    Update spot_vol_stress.py

--- portstress/core/spot_vol_stress.py
+++ portstress/core/spot_vol_stress.py
@@ -9,98 +9,52 @@ COLUMNS2 = ['post_shock_spot', 'post_shock_vol', 'quantity', 'multiplier'] + BS_
 
 GLOBAL_SHOCK_EXCLUDE_G4_GAINS = False
 
-class StressTest2:
-    '''
-        Basic Spot and Vol Stress Test
-        sticky strike: if spot changes, the implied volatility of an option with a given absolute strike does not change.
-        sticky moneyness/sticky delta: if spot changes, the implied volatility of an option with a given moneyness does not change
-
-    '''
-
-    def shock_df(self, data: DataFrame, name):
-        if not set(COLUMNS2).issubset(set(data.columns)):
-            raise Exception(f'Input data should include columns: {COLUMNS2}')
-        data[name] = np.vectorize(self.shock)(**{col: data[col] for col in COLUMNS2})
-        return data
-
-    @staticmethod
-    def shock(
-        post_shock_spot, post_shock_vol, spot, vol, quantity, multiplier, strike, put_call, time_to_expiry, rate,
-        cost_of_carry_rate, **kwargs
-    ):
-        # Pre shock valuation
-        pre_shock = bs_pricing(
-            strike=strike,
-            time_to_expiry=time_to_expiry,
-            spot=spot,
-            rate=rate,
-            vol=vol,
-            put_call=put_call,
-            cost_of_carry_rate=cost_of_carry_rate
-        )
-
-        # Post shock valuation
-        post_shock = bs_pricing(
-            strike=strike,
-            time_to_expiry=time_to_expiry,
-            spot=post_shock_spot,
-            rate=rate,
-            vol=post_shock_vol,
-            put_call=put_call,
-            cost_of_carry_rate=cost_of_carry_rate
-        )
-        return (post_shock - pre_shock) * quantity * multiplier
-
-class StressTest:
-    '''
-        Basic Spot and Vol Stress Test
-        This class only supports the "sticky strike" rule.
-    '''
-
-    def shock_df(self, data: DataFrame, name):
-        if not set(COLUMNS).issubset(set(data.columns)):
-            raise Exception(f'Input data should include columns: {COLUMNS}')
-        data[name] = np.vectorize(self.shock)(**{col: data[col] for col in COLUMNS})
-        return data
-
-    @staticmethod
-    def shock(
-        spot_shock, vol_shock, spot, quantity, multiplier, vol, strike, put_call, time_to_expiry, rate,
-        cost_of_carry_rate, vol_shock_mode: str = 'relative', **kwargs
-    ):
-        '''
-        vol_shock_mode:
-            relative: vol = vol * (1 + vol_shock); 
-            absolute: vol = vol + vol_shock;
-        '''
-        # Pre shock valuation
-        pre_shock = bs_pricing(
-            strike=strike,
-            time_to_expiry=time_to_expiry,
-            spot=spot,
-            rate=rate,
-            vol=vol,
-            put_call=put_call,
-            cost_of_carry_rate=cost_of_carry_rate
-        )
-        # Calculate post shock spot and vol
-        if vol_shock_mode == 'absolute':
-            post_shock_vol = vol + vol_shock
-        else:
-            post_shock_vol = vol * (1 + vol_shock)
-        post_shock_spot = spot * (1 + spot_shock)
-        # Post shock valuation
-        post_shock = bs_pricing(
-            strike=strike,
-            time_to_expiry=time_to_expiry,
-            spot=post_shock_spot,
-            rate=rate,
-            vol=post_shock_vol,
-            put_call=put_call,
-            cost_of_carry_rate=cost_of_carry_rate
-        )
-        return (post_shock - pre_shock) * quantity * multiplier
-
+ Image class StressTest2:
+    '''
+        Basic Spot and Vol Stress Test
+        sticky strike: if spot changes, the implied volatility of an option with a given absolute strike does not change.
+        sticky moneyness/sticky delta: if spot changes, the implied volatility of an option with a given moneyness does not change
+        time_to_expiry_adj: set an adjustment to post_shock_time_to_expiry. e.g. time_to_expiry_adj = -1/365 indicates the shock happens tomorrow
+                            this allows to capture the theta impact
+ 
+    '''
+    def __init__(self, time_to_expiry_adj:float = 0):
+        self.time_to_expiry_adj = time_to_expiry_adj
+ 
+    def shock_df(self, data: DataFrame, name):
+        if not set(COLUMNS2).issubset(set(data.columns)):
+            raise Exception(f'Input data should include columns: {COLUMNS2}')
+        data[name] = np.vectorize(self.shock)(**{col: data[col] for col in COLUMNS2})
+        return data
+ 
+    def shock(self,
+        post_shock_spot, post_shock_vol, spot, vol, quantity, multiplier, strike, put_call, time_to_expiry, rate,
+        cost_of_carry_rate, **kwargs
+    ):
+        # Pre shock valuation
+        pre_shock = bs_pricing(
+            strike=strike,
+            time_to_expiry=time_to_expiry,
+            spot=spot,
+            rate=rate,
+            vol=vol,
+            put_call=put_call,
+            cost_of_carry_rate=cost_of_carry_rate
+        )
+ 
+        post_shock_time_to_expiry = time_to_expiry + self.time_to_expiry_adj
+ 
+        # Post shock valuation
+        post_shock = bs_pricing(
+            strike=strike,
+            time_to_expiry=post_shock_time_to_expiry,
+            spot=post_shock_spot,
+            rate=rate,
+            vol=post_shock_vol,
+            put_call=put_call,
+            cost_of_carry_rate=cost_of_carry_rate
+        )
+        return (post_shock - pre_shock) * quantity * multiplier
 
 class StressTree:
     '''
