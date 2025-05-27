43be7bca224b55d11721593c1ca62c52f1e57523
yue.bai <bilal.yue.bai@gmail.com>
2025-05-27 11:10:26 +0800

    3.5.5

--- portstress/core/__init__.py
+++ portstress/core/__init__.py
@@ -3,5 +3,6 @@ from . import vol_surface_stress
 from . import black_scholes
 from . import utils
 from . import vol_curves
+from . import pnl_attribution
 
 __all__ = ['spot_vol_stress', 'vol_surface_stress', 'black_scholes', 'utils', 'vol_curves']
